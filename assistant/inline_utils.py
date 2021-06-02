ee17b35c6787af45f21cb9846361304632addf9f
itsrxns <51463109+itsrxns@users.noreply.github.com>
2021-06-02 16:36:47 +0200

    Update inline_utils.py

--- assistant/inline_utils.py
+++ assistant/inline_utils.py
@@ -541,7 +541,7 @@ async def give_plugin_cmds(client, cb):
     if cb.matches[0].group(2) == "False":
         is_official = False
     cmd_list = CMD_LIST if is_official else XTRA_CMD_LIST
-    help_string = f"**💡 PLUGIN NAME 💡 :** `{plugin_name}` \n{cmd_list[plugin_name]}"
+    help_string = f"**NOME PLUGIN:** `{plugin_name}` \n{cmd_list[plugin_name]}"
     help_string += "\n\n**(C) @FRIDAYOT** ".format(plugin_name)
     await cb.edit_message_text(
         help_string,
